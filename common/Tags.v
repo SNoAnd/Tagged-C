a5f8c719cedd60e04a6bbbb97a8d55ea5488a551
Sean Anderson <ander28@pdx.edu>
2022-12-29 11:33:56 -0800

    PNVI

--- common/Tags.v
+++ common/Tags.v
@@ -160,3 +160,63 @@ Module PVI <: Policy.
     
   Definition DummyT (ts : list tag) : option (list tag) := Some ts.
 End PVI.
+
+Module PNVI <: Policy.
+  
+  Inductive myTag :=
+  | Glob (id:ident)
+  | Dyn (c:nat)
+  | X
+  .
+
+  Definition tag := myTag.
+
+  Theorem tag_eq_dec : forall (t1 t2:tag), {t1 = t2} + {t1 <> t2}.
+  Proof.
+    unfold tag. intros. repeat decide equality.
+  Qed.
+  Definition def_tag := X.
+
+  Definition InitPCT := Dyn 0.
+
+  Definition GlobalT (id : ident) := Glob id.
+  
+  Definition LocalT (align : nat) (pct : tag) : option (tag * tag * list tag) :=
+    match pct with
+    | Dyn c =>
+        Some (Dyn (S c), Dyn c, repeat (Dyn c) align)
+    | _ =>
+        None
+    end.
+
+  Definition VarT (pct pt : tag) : option tag := Some pt.
+
+  Definition ConstT : tag := X.
+  
+  Definition UnopT (pct vt : tag) : option (tag * tag) := Some (pct, vt).
+
+  Definition BinopT (pct vt1 vt2 : tag) : option (tag * tag) :=
+    match vt1, vt2 with
+    | Dyn n, X =>  Some (pct, vt1)
+    | Glob id, X => Some (pct, vt1)
+    | _, _ => Some (pct, vt2)
+    end.
+
+  Definition LoadT (pct pt vt: tag) (lts : list tag) : option tag :=
+    match pt with
+    | X => None
+    | _ => if forallb (tag_eq_dec pt) lts then Some vt else None
+    end.
+
+  Definition StoreT (pct pt ovt vt : tag) (lts : list tag) : option (tag * tag * list tag) :=
+    match pt with
+    | X => None
+    | _ => if forallb (tag_eq_dec pt) lts then Some (pct,vt,lts) else None
+    end.
+
+  Definition IfSplitT (pct vt : tag) : option (tag * tag) := Some (pct, pct).
+
+  Definition IfJoinT (pct opct : tag) : option tag := Some pct.
+    
+  Definition DummyT (ts : list tag) : option (list tag) := Some ts.
+End PNVI.
