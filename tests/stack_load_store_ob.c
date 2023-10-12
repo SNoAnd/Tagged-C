12c2b1a29582cd422a7724464f7b15dea53f1344
Sean Anderson <snanders@pdx.edu>
2023-10-12 02:49:17 -0700

    Out of bounds test that doesn't get stuck

---
+++ tests/stack_load_store_ob.c
@@ -0,0 +1,12 @@
+int main() {
+	int x[3];
+	int y[3];
+
+	// Default layout: SP                          Base
+	//                  | [y0][y1][y2][x0][x1][x2] |
+	for (int i = 2; i > -2; i--) {
+		x[i] = i;
+	}
+
+	return y[4];
+}
