824c431a0204c3f1d44e13fc8d0fed7faeea6a50
Allison <naak@pdx.edu>
2023-11-01 17:09:25 -0700

    adding fgets test, has no errors.

--- tests/double_free_basic_input_handlabelled.c
+++ tests/double_free_basic_input_handlabelled.c
@@ -9,7 +9,7 @@
 #define MAX_SIZE 1024
 int main() {
     char* input = (char*) malloc(MAX_SIZE * sizeof(char));
-    printf("enter some input:");
+    printf("enter some input:\n");
     // FILE *fp; instead of stdin if needed.
     // stdin's fp is known by stdlib  
     fgets(input, MAX_SIZE, stdin);
@@ -17,5 +17,5 @@ int main() {
     label1: free(input); // the most straightforward double free
     // fuzzer should not get here
     printf("You entered %s. Hope it doesn't have a problem!", input);
-	exit(EXIT_SUCCESS);
+	return EXIT_SUCCESS;
 }
